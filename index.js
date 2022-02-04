875f8660db0c184ae51c6449506e4b548302c2c1
Salvija Dargyte <salvija13@gmail.com>
2022-02-04 15:44:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,108 @@
-const natureNames = [
-  "Nature",
-  "Beach",
-  "Desert",
-  "Forest",
-  "Meadow",
-  "Mountains",
-  "Ocean",
-  "Snow"
+class Image {
+  constructor(src, bgPosition) {
+    this.src = src;
+    this.bgPosition = bgPosition;
+  }
+}
+
+class Nature {
+  constructor(name) {
+    this.name = name;
+    this.images = [];
+  }
+
+  addImages(images, nature = "") {
+
+    images.forEach(([image, bgPosition]) => {
+      const src = nature ? `media/${nature}/${image}` : `media/${image}`
+
+      const imageObj = new Image(src, bgPosition || "")
+
+      this.images.push(imageObj)
+    });
+
+    return this;
+  }
+}
+
+const natures = [
+  new Nature("Nature").addImages([
+    ["flower.jpg"]
+  ], ""),
+
+  new Nature("Beach").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "beach"),
+
+  new Nature("Desert").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "desert"),
+
+  new Nature("Forest").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "forest"),
+
+  new Nature("Meadow").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "meadow"),
+
+  new Nature("Mountains").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "mountains"),
+
+  new Nature("Ocean").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "ocean"),
+
+  new Nature("Snow").addImages([
+    ["1.jpg"], ["2.jpg"], ["3.jpg"]
+  ], "snow"),
 ]
+
+
+const fullPageEl = document.getElementById("fullpage");
+
+const createSlides = (images) => images.map(image => {
+  const slide = document.createElement("div")
+  slide.classList.add("slide")
+
+  slide.style.backgroundImage = `url(${image.src})`
+  slide.style.backgroundPosition = image.bgPosition
+
+  return slide
+})
+
+const createSection = (nature) => {
+  const section = document.createElement("section")
+  section.classList.add("section")
+
+  const slides = createSlides(nature.images)
+
+  slides.forEach(slide => section.appendChild(slide))
+
+  const title = document.createElement("h1")
+  title.classList.add("title")
+  title.innerText = nature.name
+
+  section.appendChild(title)
+
+  return section
+
+}
+
+natures.forEach(nature => {
+  const section = createSection(nature)
+
+  fullPageEl.appendChild(section)
+})
+
+
+const natureNames = natures.map(nature => nature.name)
 
 new fullpage("#fullpage", {
   navigation: true,
   navigationPosition: "right",
   showActiveTooltip: true,
   navigationTooltips: natureNames,
+  anchors: ["one", "two", "three"]
 })
